27117a1608c8c5bf9dd6327a8e43cfb1eb709917
littleqz <qizhihere@gmail.com>
2016-08-18 17:35:29 +0800

    Fix errors.

--- install.sh
+++ install.sh
@@ -2,7 +2,8 @@
 
 PWD=$(dirname "$0")
 cd "$PWD" && {
-    git subtree pull --prefix=bundle/Vundle.vim vundle master
+    git subtree pull --prefix=bundle/Vundle.vim \
+        https://github.com/VundleVim/Vundle.vim master
     if [ "$(ls -1 bundle/ | grep -v vundle | wc -l)" -eq 0 ]; then
         vim +BundleInstall +qall
     fi 2>/dev/null
