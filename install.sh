cc1ca03e32d14bc210a6712be7f45785f0ca8659
littleqz <qizhihere@gmail.com>
2016-08-18 17:05:28 +0800

    Fix bugs.

--- install.sh
+++ install.sh
@@ -1,6 +1,9 @@
 #!/usr/bin/env sh
 
-PWD="$(dirname "$0")"
-if [ "$(ls -1 ${PWD}/bundle/ | grep -v vundle | wc -l)" -eq 0 ]; then
-    vim +BundleInstall +qall
-fi 2>/dev/null
+PWD=$(dirname "$0")
+cd "$PWD" && {
+    git submodule update --init --recursive &&
+    if [ "$(ls -1 bundle/ | grep -v vundle | wc -l)" -eq 0 ]; then
+        vim +BundleInstall +qall
+    fi 2>/dev/null
+}
