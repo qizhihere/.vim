2ce84dbd5dbd45679afd9487803f5c51ea642d01
littleqz <qizhihere@gmail.com>
2016-08-18 17:33:18 +0800

    Replace submodule with subtree.

--- install.sh
+++ install.sh
@@ -2,7 +2,7 @@
 
 PWD=$(dirname "$0")
 cd "$PWD" && {
-    git submodule update --init --recursive &&
+    git subtree pull --prefix=bundle/Vundle.vim vundle master
     if [ "$(ls -1 bundle/ | grep -v vundle | wc -l)" -eq 0 ]; then
         vim +BundleInstall +qall
     fi 2>/dev/null
