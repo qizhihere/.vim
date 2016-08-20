28a563de9e9616149ccb9f95c42d413c50381ece
littleqz <qizhihere@gmail.com>
2016-08-20 11:05:04 +0800

    Modify install script.

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env sh
 
-PWD=$(dirname "$0")
+PWD=$(realpath "$(dirname "$0")")
 cd "$PWD" && {
     git pull origin master
     git subtree pull --squash --prefix=bundle/Vundle.vim \
@@ -8,4 +8,11 @@ cd "$PWD" && {
     if [ "$(ls -1 bundle/ | grep -v Vundle.vim | wc -l)" -eq 0 ]; then
         vim +BundleInstall +qall
     fi 2>/dev/null
+
+    [ "$PWD" != "$HOME/.vim" ] && {
+        cmd="ln -s \"$PWD\" \"$HOME/.vim\""
+        echo
+        echo $cmd
+        eval "$cmd"
+    }
 }
